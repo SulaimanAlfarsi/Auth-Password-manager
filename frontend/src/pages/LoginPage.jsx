da255a3b6a5617669045830ca8c442254d878304
Sulaiman Al-Farsi <132258182+SulaimanAlfarsi@users.noreply.github.com>
2025-09-24 23:00:02 +0400

    Update Login page UI

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from "framer-motion";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
+
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const isLoading = false;
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+ 
+  };
+
+
   return (
-    <div>
-      
-    </div>
+   <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+    >
+
+     <div className='p-8'>
+        <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-[#ec9569]  to-[#EA6601] text-transparent bg-clip-text'>
+          Welcome Back
+        </h2>
+
+       <form onSubmit={handleLogin} className="space-y-6">
+
+        <Input
+          icon={Mail}
+          type='email'
+          placeholder='Email Address'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          icon={Lock}
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <div className='flex items-center mb-6'>
+						<Link to='/forgot-password' className='text-sm text-[#f38650] hover:underline'>
+							Forgot password?
+						</Link>
+					</div>
+
+        <motion.button
+                className='mt-5 w-full py-3 px-4 bg-[#f38650]  text-white 
+                font-bold rounded-lg shadow-lg hover:bg-[#EA6601]
+                hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-amber-600 focus:ring-offset-2
+                focus:ring-offset-gray-900 transition duration-200'
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+                type='submit'
+                disabled={isLoading}
+                >
+                 {isLoading ? <Loader className='w-6 h-6 animate-spin  mx-auto' /> : "Login"}
+
+                </motion.button>
+
+       </form>
+      </div>
+
+      <div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+        <p className='text-sm text-gray-400'>
+          Don't have an account?{" "}
+          <Link to={"/signup"} className='text-[#EA6601] hover:underline'>
+            Sign Up
+          </Link>
+        </p>
+      </div>
+     
+    </motion.div>
   )
 }
 
